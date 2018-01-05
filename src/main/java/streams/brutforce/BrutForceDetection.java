e319b70038969f056342acd3eeaa458a8f516f52
Volodymyr.Pogorelov <Volodymyr.Pogorelov@gmail.com>
2018-01-05 20:29:41 +0200

    Properties for producer (initial)

--- src/main/java/streams/brutforce/BrutForceDetection.java
+++ src/main/java/streams/brutforce/BrutForceDetection.java
@@ -1,4 +1,12 @@
 package streams.brutforce;
 
+import java.util.Properties;
+
 public class BrutForceDetection {
+
+    public static void main(String[] args){
+        Properties props = new Properties();
+        props.put("metadata.broker.list", "");
+    }
+
 }
